5dbc595fd771f9da3f2bbd38ac7d04841a4311af
DESKTOP-G03N79N\csaba.szasz <cs.szasz@gmail.com>
2021-10-24 09:20:33 +0300

    made basic layout for Quiz

--- app/src/main/java/com/example/quizapplication/MainActivity.kt
+++ app/src/main/java/com/example/quizapplication/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.quizapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun buttonClickEvent(view: View) {
+        Log.i("Info", "Button Pressed");
+
+        val text = "Button Pressed Toast!"
+        val duration = Toast.LENGTH_SHORT
+
+        val toast = Toast.makeText(applicationContext, text, duration)
+        toast.show()
     }
 }
